f530a127d4a55f0ea0862824a08cad568b480750
Renan Huanca <renan.huanca@mojix.com>
2019-01-16 11:47:22 -0400

    modified script to call cxi tests

--- run-jmeter.sh
+++ run-jmeter.sh
@@ -1,23 +1,27 @@
-#! /bin/sh
+#!/bin/bash
 
-echo "############### Clean Folders ###############"
-rm -rf ../test1/ *../test2/* ../compress.tar.gz /tmp/compress.tar.gz /tmp/results.tar.gz /tmp/Latency.csv /tmp/RequestData.xml
-sleep 3
-echo "############### Run Jmeter ###############"
-./jmeter.sh -n -t ${1} -l ../test1/vizix.jtl -e -o ../test2/
-sleep 3
-cd ..
-sleep 1
-echo "############### Compres Results ###############"
-tar -czvf compress.tar.gz test2/
-sleep 3
-echo "############### Copy Results in Tmp folder ###############"
-cp compress.tar.gz /tmp
-sleep 1
-echo "###############Compress Results and Data ###############"
-cd /tmp
-sleep 1
-tar -czvf results.tar.gz compress.tar.gz Latency.csv RequestData.xml
-echo "####### HTTP Server http://40.76.3.235:9050/compress.tar.gz ######"
-sleep 2
-python -m SimpleHTTPServer 9050
+if [[ -z "${JMETER_HOME}" ]]; then
+  echo "Please set JMETER_HOME system env variable"
+  exit 0
+else
+  jmeter_path="${JMETER_HOME}"
+fi
+
+test_plan="cxi_endpoints.jmx"
+server="staging.vizix.io"
+
+echo
+echo "test plan: $test_plan"
+echo "server: $server"
+echo
+
+echo "cleaning results folder..."
+rm -rf results
+
+echo "running jmeter on $jmeter_path"
+echo
+#                   ./jmeter.sh -n -t ${1} -l ../test1/vizix.jtl -e -o ../test2/
+cmd="$jmeter_path/bin/jmeter.sh -n -Jpage=$server -t $test_plan -l vizix.jtl -e -o results/" 
+echo "$cmd"
+$cmd
+#$jmeter_path/bin/jmeter.sh -n -t  -Jpage=$server -l cxiapi.jtl -e -o results/ $test_plan
